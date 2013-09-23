304525d78b7fc880c30ede1225cbe5cc4f977d9f
Michael Klopf <klopf.michael@gmail.com>
2013-09-23 21:55:46 +0200

    Added bootstrap-datepicker for input.

--- static/js/input.js
+++ static/js/input.js
@@ -1,4 +1,10 @@
 // jQuery code
 $(document).ready(function() {
+    $('#dp1').datepicker({
+      format: 'dd.mm.yyyy'
+    });
     
+    $('#dp2').datepicker({
+      format: 'dd.mm.yyyy'
+    });
 });
