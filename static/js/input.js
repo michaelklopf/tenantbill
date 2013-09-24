5570b879034cccce8d191efe14478f5f4ddb4f7e
Michael Klopf <klopf.michael@gmail.com>
2013-09-24 22:00:25 +0200

    Added more input fields and began jquery.

--- static/js/input.js
+++ static/js/input.js
@@ -7,4 +7,15 @@ $(document).ready(function() {
     $('#dp2').datepicker({
       format: 'dd.mm.yyyy'
     });
+    
+    $('#sendbutton').click(function() {
+        var data = new Object();
+        data.date1 = $('#dp1').val();
+        data.date2 = $('#dp2').val();
+        data.totalCost = $('#totalCost').val();
+        data.prePay = $('#prePay').val();
+        data.noCost = $('#noCost').val();
+        console.log(data);
+    });
+    
 });
