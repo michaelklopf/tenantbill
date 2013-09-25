2942c818129b04e0a0e9c488a2387e3481c86ef0
Michael Klopf <klopf.michael@gmail.com>
2013-09-25 21:54:41 +0200

    Added input fields validation for dates.

--- static/js/input.js
+++ static/js/input.js
@@ -9,13 +9,44 @@ $(document).ready(function() {
     });
     
     $('#sendbutton').click(function() {
+        if (areCheckFieldsOk()) {
+            $('#contentarea').text('');
+            var data = createObject();
+        } else {
+            return;
+        }
+    });
+    
+    var createObject = function() {
         var data = new Object();
         data.date1 = $('#dp1').val();
         data.date2 = $('#dp2').val();
         data.totalCost = $('#totalCost').val();
         data.prePay = $('#prePay').val();
         data.noCost = $('#noCost').val();
-        console.log(data);
-    });
+        return data;
+    }
+    
+    var isCheckDateOk = function(date) {
+        var regex = /^\d{2}\.\d{2}\.\d{4}$/;
+        return regex.test(date);    
+    }
+    
+    var areCheckFieldsOk = function() {
+        console.log($('#dp1').val());
+        console.log(isCheckDateOk($('#dp1').val()));
+        if(!isCheckDateOk($('#dp1').val()) || !isCheckDateOk($('#dp2').val())) {
+            alertMessage();
+            return false;
+        } else if {
+            // TODO check the number fields and check the year between the dates
+        }
+        return true;
+    }
+    
+    var alertMessage = function() {
+        $('#contentarea').text('Please check your inputs again.');
+        // TODO Make nice message and add red highlight to inputs
+    }
     
 });
